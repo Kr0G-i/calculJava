35c61779385dede3c46c97c0c0e78e2d51377760
Kr0G-i <73434509+Kr0G-i@users.noreply.github.com>
2021-03-25 10:35:09 +0100

    iniProjet

---
+++ src/calculJava/CalculsTest.java
@@ -0,0 +1,14 @@
+package calculJava;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
